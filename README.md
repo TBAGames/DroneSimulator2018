3978fac1f4283c37bac1473a0a0ec057f339c1b0
LAPTOP-ARAIKUT4\Aaron <ramosa_aa@hotmail.com>
2018-12-06 14:32:02 -0500

    Added planar boundary, buildings, and textures to buildings

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DroneSimulator2018
+
+### RESOURCES ###
+
+Asplhalt Texture
+https://opengameart.org/content/white-asphalt-tilling-512px
+
+Building Brick Texture
+https://opengameart.org/content/small-red-brick-wall-256px
--- textured_material_vp.glsl
+++ textured_material_vp.glsl
@@ -12,21 +12,15 @@ uniform mat4 view_mat;
 uniform mat4 projection_mat;
 uniform mat4 normal_mat;
 
-// Toon setting
-uniform int toon_enabled;
-
 // Attributes forwarded to the fragment shader
 out vec3 position_interp;
 out vec3 normal_interp;
 out vec4 color_interp;
 out vec2 uv_interp;
-out vec3 light_pos1;
-out vec3 light_pos2;
-//out int toon_on;
+out vec3 light_pos;
 
 // Material attributes (constants)
-uniform vec3 light_position1 = vec3(-0.5, -0.5, 1.5);
-uniform vec3 light_position2 = vec3(-0.25, 1.0, -0.5);
+uniform vec3 light_position = vec3(-0.5, -0.5, 1.5);
 
 
 void main()
@@ -41,8 +35,5 @@ void main()
 
     uv_interp = uv;
 
-    light_pos1 = vec3(view_mat * vec4(light_position1, 1.0));
-	light_pos2 = vec3(view_mat * vec4(light_position2, 1.0));
-
-	//toon_on = toon_enabled;
+    light_pos = vec3(view_mat * vec4(light_position, 1.0));
 }
