89acef947814d7d47879bb9f257d369d7afab02a
LAPTOP-ARAIKUT4\Aaron <ramosa_aa@hotmail.com>
2018-12-07 18:13:52 -0500

    Changed speed constants

--- flame_vp.glsl
+++ flame_vp.glsl
@@ -17,8 +17,8 @@ out float particle_id;
 
 // Simulation parameters (constants)
 uniform vec3 up_vec = vec3(0.0, 1.0, 0.0); // Up direction
-float accel = 3.9; // An acceleration applied to the particles coming from some attraction force
-float speed = 1.5; // Control the speed of the motion
+float accel = 8.9; // An acceleration applied to the particles coming from some attraction force
+float speed = 8.0; // Control the speed of the motion
 
 // Define some useful constants
 const float pi = 3.1415926536;
@@ -34,32 +34,22 @@ void main()
     // Define particle id
     particle_id = color.r; // Derived from the particle color. We use the id to keep track of particles
 
-    // Define time in a cyclic manner
-    float phase = two_pi*particle_id; // Start the sin wave later depending on the particle_id
-    float param = timer / 10.0 + phase; // The constant that divides "timer" also helps to adjust the "speed" of the fire
-    float rem = mod(param, pi_over_two); // Use the remainder of dividing by pi/2 so that we are always in the range [0..pi/2] where sin() gives values in [0..1]
-    float circtime = sin(rem); // Get time value in [0..1], according to a sinusoidal wave
-                                    
-    // Set up parameters of the particle motion
-    float t = abs(circtime)*(0.3 + abs(normal.y)); // Our time parameter
-
-	// Get cone vector based on particle id
-	float z = sin(particle_id / id_bounds.y);
-	float phi = 180.0 + particle_id - (two_pi * floor (particle_id/two_pi));
-
-	float z_xy_component = 1 - (z * z);
-
-	vec3 cone_vec;
-	cone_vec.x = z_xy_component * cos(phi) + circtime * cos(phase);
-	cone_vec.y = z_xy_component * sin(phi);
-	cone_vec.z = z + circtime*sin(t)/4.0;
+	float arc = two_pi * (particle_id / id_bounds.y );
 
 	// Get tradjectory vector
-	vec3 direction = cone_vec - up_vec;
+	vec3 direction = vec3(cos(arc), sin(arc), 0.0);
+
+	float phase = two_pi*particle_id; // Start the sin wave later depending on the particle_id
+	float param = timer / 10.0 + phase; // The constant that divides "timer" also helps to adjust the "speed" of the fire
+	float rem = mod(param, pi_over_two); // Use the remainder of dividing by pi/2 so that we are always in the range [0..pi/2] where sin() gives values in [0..1]
+	float circtime = sin(rem); // Get time value in [0..1], according to a sinusoidal wave
+
+	// Set up parameters of the particle motion
+	float t = abs(circtime)*(0.3 + abs(normal.y)); // Our time parameter
 
     // First, work in local model coordinates (do not apply any transformation)
     vec3 position = vertex;
-    position += speed*direction*accel*t*t; // Particle moves up
+    position += speed*direction*vec3(1.0, 30.0, 20.0)*accel*t*t; // Particle moves up
     
     // Define output position but do not apply the projection matrix yet
     gl_Position = view_mat * world_mat * vec4(position, 1.0);
